402fb5007052a3f79e05f2636209ab1df17c3674
derekprieur <DPrieur123@gmail.com>
2023-02-15 07:35:01 -0500

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
 
       // Save user to mongodb
       if (profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://refine-property-dashboard.onrender.com/api/v1/users', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -114,7 +114,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://refine-property-dashboard.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
